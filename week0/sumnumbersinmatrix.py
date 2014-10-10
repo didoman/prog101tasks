b3f57b0d98bd624a0986004c0e948ff766da3c0b
Deyan Vasilev <didoman@gbg.bg>
2014-10-10 20:35:39 +0300

    matrix bombing and a fix for sum numbers in matrix

--- week0/sumnumbersinmatrix.py
+++ week0/sumnumbersinmatrix.py
@@ -1,15 +1,16 @@
 # sum numbers in a NxM matrix
 
+
 def sum_matrix(m):
     thesum = 0
     for i in range(0,len(m)):
-        subma3x = m[i]
-        for j in range(0,len(subma3x)):
-            thesum = thesum + subma3x[j]
+        for j in range(0,len(m[i])):
+            thesum = thesum + m[i][j]
 
-    print(thesum)
+    print("sum of matrix numbers",thesum)
     return thesum
 
-sum_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-sum_matrix([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
-sum_matrix([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+if __name__ == '__main__':
+    sum_matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    sum_matrix([[0, 0, 0], [0, 0, 0], [0, 0, 0]])
+    sum_matrix([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
