cca43df136cfdd0e006f03c704391ba7c8825085
Deyan Vasilev <didoman@gbg.bg>
2014-10-10 13:58:35 +0300

    another batch of tasks for prog101

---
+++ week0/numbertolist.py
@@ -0,0 +1,10 @@
+#turn a number into a list of digits
+
+def number_to_list(n):
+    print(list(str(n)))
+    return list(str(n))
+    #yep. this easy
+
+number_to_list(123)
+number_to_list(99999)
+number_to_list(123023)
