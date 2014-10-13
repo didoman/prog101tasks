d3b95bb7140e7c1b44354f75476f73e1c03b6aae
Deyan Vasilev <didoman@gbg.bg>
2014-10-13 18:54:20 +0300

    taxonomy and fix for spam and eggs

--- week0/hard/spamandeggs.py
+++ week0/hard/spamandeggs.py
@@ -3,32 +3,28 @@
 from math import sqrt
 
 def prepare_meal(number):
-	output = ""
-	largest_divisor = 1
-	
-	for i in range (1, number): # if no integer up to the square root of Number is a valid divisor, the Number is prime.
-		if number % i == 0:
-			#print(number / i, 3**i)
-			largest_divisor = int(number/i)
-		if 3**i == largest_divisor:
-			
-			output = i*("spam ")
-			break
-	
-	if i % 3 == 0 and len(output) !=0 :
-		output = output + "and eggs"
-	elif len(output) == 0:
-		output = output + "eggs"
-			
-	print(output)
+    output = ""
+    largest_divisor = 1
+
+    for i in range (1, number): 
+        if number % 3**i == 0:
+            output = i*"spam "
+
+
+    if number % 5 == 0 and len(output) !=0 :
+        output = output + "and eggs"
+    elif len(output) == 0:
+        output = output + "eggs"
+
+    print(output)
 	
 	
 prepare_meal(3)
 prepare_meal(9)
 prepare_meal(27)
-prepare_meal(81)
-prepare_meal(5)
-prepare_meal(15)
-prepare_meal(45)
-prepare_meal(360)
-prepare_meal(30)
+# prepare_meal(81)
+prepare_meal(5) # eggs
+prepare_meal(15) # spam and eggs , because 15 / 3 and 3**1 = 3 => 1x spam
+prepare_meal(45) # spam spam and eggs , because 45 / 9 and 3**2 = 9 => 2x spam
+# prepare_meal(360)
+# prepare_meal(30)
